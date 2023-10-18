64db01a1de083640138829f0d7eb51279eecf6eb
khanghy1000 <khanghy130603@gmail.com>
2023-10-18 19:09:22 +0700

    Add user validate schema

---
+++ src/schemas/User.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+    username: z.string().min(5),
+    password: z.string().min(7),
+    role: z.enum(["user", "admin"]),
+});
+
+export const NoPasswordUserSchema = UserSchema.omit({ password: true });
+
+export type User = z.infer<typeof UserSchema>;
+
+export type NoPasswordUser = z.infer<typeof NoPasswordUserSchema>;
