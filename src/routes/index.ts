c2742f739951084055a428fdf1276b9a258d4384
khanghy1000 <khanghy130603@gmail.com>
2023-10-18 17:38:32 +0700

    Use book and book instance routers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import authorRouter from "./authorRoutes";
 import genreRouter from "./genreRoutes";
+import bookRouter from "./bookRoutes";
+import BookInstanceRouter from "./bookInstanceRoutes";
 
 const router = express.Router();
 
 router.use("/api/authors", authorRouter);
 router.use("/api/genres", genreRouter);
-
+router.use("/api/books", bookRouter);
+router.use("/api/bookinstances", BookInstanceRouter);
 export default router;
 
 // catalog/ — The home/index page.
